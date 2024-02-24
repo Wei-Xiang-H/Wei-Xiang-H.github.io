4f4f4ddfe86b6581b747fc08acb7c2f28adee729
wei xiang <qwerc232692@gmail.com>
2024-02-25 01:46:05 +0800

    預定頁

---
+++ RentJoy/RentJoy_Reserved.js
@@ -0,0 +1,8 @@
+$("#nextStepBtn").click(function () {
+    $(".step1").toggle();
+    $(".step2").toggle();
+})
+$("#backToStep1").click(function () {
+    $(".step1").toggle();
+    $(".step2").toggle();
+})
